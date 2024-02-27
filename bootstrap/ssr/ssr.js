36e24e3a8d94a84af6f3b8dbdee860c657874f07
Herbet Junior <herbetjr@gmail.com>
2024-02-27 11:15:25 -0300

    node

--- bootstrap/ssr/ssr.js
+++ bootstrap/ssr/ssr.js
@@ -491,7 +491,7 @@ createServer(
     page,
     render: renderToString,
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, /* @__PURE__ */ Object.assign({ "./Pages/Colaborador/Acompanhamento.vue": () => import("./assets/Acompanhamento-Bt-FnEYS.js"), "./Pages/Colaborador/Painel.vue": () => import("./assets/Painel-CdMPO-Bi.js"), "./Pages/Index.vue": () => import("./assets/Index-nj-iBmem.js") })),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, /* @__PURE__ */ Object.assign({ "./Pages/Colaborador/Acompanhamento.vue": () => import("./assets/Acompanhamento-Bt-FnEYS.js"), "./Pages/Colaborador/Painel.vue": () => import("./assets/Painel-CdMPO-Bi.js"), "./Pages/Index.vue": () => import("./assets/Index-s469hM3D.js") })),
     setup({ App, props, plugin }) {
       return createSSRApp({ render: () => h$1(App, props) }).use(plugin).use(P, {
         ...page.props.ziggy,
